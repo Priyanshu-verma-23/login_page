f5f347058d44e7b16f8c13280d71bbc84d876a4f
priyanshu <priyanshuverma7245@gmail.com>
2024-01-09 13:58:26 +0530

    today

---
+++ script.js
@@ -0,0 +1,12 @@
+var goLion = document.getElementById("golion")
+var onLion = document.getElementById("slion")
+
+function Liongya(){
+    goLion.style.display = "block"
+    onLion.style.display = "none"
+}
+
+function Lionaya(){
+    goLion.style.display = "none"
+    onLion.style.display = "block"
+}
